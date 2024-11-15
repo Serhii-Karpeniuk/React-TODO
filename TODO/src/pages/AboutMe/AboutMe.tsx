ee962a42c9479428161620a3e9ba83ee5233846d
Serhii-Karpeniuk <142886841+Serhii-Karpeniuk@users.noreply.github.com>
2024-11-16 00:18:00 +0200

    add about me information

--- TODO/src/pages/AboutMe/AboutMe.tsx
+++ TODO/src/pages/AboutMe/AboutMe.tsx
@@ -1,7 +1,15 @@
+import "./AboutMe.scss";
+
 const AboutMe = () => {
   return (
     <div>
-      <p>My Name is Serhii</p>
+      <p className="aboutMe">
+        My name is Serhii, I'm 21 years old. <br />
+        I'm a web developer and I'm actively learning React. <br />I have
+        experience with JavaScript, HTML, CSS, and other technologies needed to
+        create modern web applications. <br />I am constantly improving my
+        skills and looking for new opportunities for development in this field.
+      </p>
     </div>
   );
 };
